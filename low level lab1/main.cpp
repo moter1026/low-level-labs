f194ba647bcadf9d5b464a46bb333ef67dd229f0
moter1026 <matvej.sidoroff2016@yandex.ru>
2024-10-09 02:12:50 +0400

    Код на языке C с упрощением

--- low level lab1/main.cpp
+++ low level lab1/main.cpp
@@ -11,62 +11,104 @@ void custom_swap(int* a, int* b) {
 }
 
 void ShakerSort(int* arr, unsigned int size) {
-	size_t n = size;
-	size_t start = 0;
-	size_t end = n - 1;
+	unsigned int n = size;
+	unsigned int start = 0;
+	unsigned int end = n;
+	end -= 1;
 	bool swapped;
-	while (n > 0) {
-		swapped = false;
-		for (size_t i = start; i < end; ++i) {
-			if (arr[i] > arr[i + 1]) {
-				custom_swap((arr + i), (arr + i + 1));
-				swapped = true;
-			}
-		}
-		if (swapped == false) break;
-		end--;
-		n--;
-		if (n <= 0) break;
-		for (size_t i = end; i > start; --i) {
-			if (arr[i] < arr[i - 1]) {
-				custom_swap((arr + i), (arr + i - 1));
-				swapped = true;
-			}
-		}
-		start++;
-		n--;
-		if (swapped == false) break;
+	unsigned int i = start;
+	unsigned int j = end;
+	int* tmpi;
+cycle_start:
+	if (n <= 0) return;
+	swapped = false;
+
+	i = start;
+cycle1_start:
+	if (i >= end) goto cycle1_end;
+	if (arr[i] > arr[i + 1]) {
+		tmpi = arr;
+		tmpi += i;
+		custom_swap(tmpi, tmpi + 1);
+		swapped = true;
 	}
+	i += 1;
+	goto cycle1_start;
+
+cycle1_end:
+	if (swapped == false) return;
+	end -= 1;
+	n -= 1;
+	if (n <= 0) return;
+
+	j = end;
+cycle2_start:
+	if (j <= start) goto cycle2_end;
+	if (arr[j] < arr[j - 1]) {
+		tmpi = arr;
+		tmpi += j;
+		custom_swap(tmpi, tmpi - 1);
+		swapped = true;
+	}
+	j -= 1;
+	goto cycle2_start;
+cycle2_end:
+	start += 1;
+	n -= 1;
+	if (swapped == false) return;
+	goto cycle_start;
 }
 
 unsigned int getLen(char* str) {
-	unsigned int i;
-	for (i = 0; *(str + i) != '\0'; i++) {}
-	return i;
+	unsigned int i = 0;
+cycle_start:
+	if (*(str + i) == '\0') return i;
+	i += 1;
+	goto cycle_start;
 }
 
 bool IsNumber(char* str, unsigned int len) {
-	const char numbersAndDot[11] = { '.','0','1','2','3','4','5','6','7','8','9' };
+	const char numbersAndDot[12] = { '-', '.', '0','1','2','3','4','5','6','7','8','9' };
 	bool isNum = false;
-	for (unsigned int i = 0; i < len; i++)
-	{
-		isNum = false;
-		for (unsigned int j = 0; j < 11; ++j) {
-			if (*(str + i) == *(numbersAndDot + j)) 
-			{	isNum = true; break; }
+
+	unsigned int i = 0;
+	unsigned int j = 0;
+cycle_start:
+	if (i >= len) goto cycle_end;
+	isNum = false;
+
+	j = 0;
+	cycle1_start:
+		if (j >= 12) goto cycle1_end;
+		if (*(str + i) != *(numbersAndDot + j))
+		{
+			j += 1;
+			goto cycle1_start;
 		}
-		if (isNum) continue;
-		else return false;
-	}
+		isNum = true;
+
+	cycle1_end:
+		if (!isNum) goto if_else;
+			i += 1;
+			goto cycle_start;
+		if_else:
+			return false;
+
+cycle_end:
 	return true;
 }
 
 void printArr(int* arr, unsigned int size) {
 	printf("[");
-	for (size_t i = 0; i < size; i++)
-	{
-		printf("%i; ", *(arr + i));
-	}
+
+	unsigned int i = 0;
+cycle_start:
+	if (i >= size) goto cycle_end;
+	printf("%i; ", *(arr + i));
+	i += 1;
+	goto cycle_start;
+
+cycle_end:
 	printf("]");
 }
 
@@ -77,23 +119,30 @@ int main() {
 	unsigned int len = 0;
 	printf("\nEnter array length\n");
 	scanf("%u", &len);
-	for (size_t i = 0; i < len; i++)
-	{
-		char val[MAX_STR_SIZE];
-		unsigned int strLen = 0;
-		scanf("%s", val);
-		strLen = getLen(val);
 
-		if (*val == '/') { break; }
-		else if (IsNumber(val, strLen));
-		else
-		{
-			printf("You entered a string, not a number.\nEnter an array of numbers using Enter.\nTo finish typing, enter /\n");
-			continue;
-		}
+	unsigned int strLen = 0;
+	int i = 0;
+cycle_start:
+	if (i >= len) goto cycle_end;
+	char val[MAX_STR_SIZE];
+	scanf("%s", val);
+	strLen = getLen(val);
 
-		*(arr + i) = atoi(val);
-	}
+	if (*val != '/') goto if_else;
+	goto cycle_end;
+
+if_else:
+	if(!IsNumber(val, strLen)) goto if_end;
+	*(arr + i) = atoi(val);
+	i += 1;
+	goto cycle_start;
+
+if_end:
+	printf("You entered a string, not a number.\nEnter an array of numbers using Enter.\nTo finish typing, enter /\n");
+	i -= 1;
+	goto cycle_start;
+
+cycle_end:
 
 	ShakerSort(arr, len);
 	printArr(arr, len);
